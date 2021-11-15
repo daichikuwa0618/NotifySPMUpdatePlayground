2b6e044ca924d5a4f0811dd9c392799996971f26
Daichi Hayashi <daichikuwa0618@gmail.com>
2021-11-16 08:47:46 +0900

    add initial Xcode project

---
+++ NotifySPMUpdatePlayground/NotifySPMUpdatePlayground/NotifySPMUpdatePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotifySPMUpdatePlaygroundApp.swift
+//  NotifySPMUpdatePlayground
+//
+//  Created by Daichi Hayashi on 2021/11/16.
+//
+
+import SwiftUI
+
+@main
+struct NotifySPMUpdatePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
